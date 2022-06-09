aa13c85ce5255282ed5c9f50f45741d3a9a1fe40
Georgi Martsenkov <g.martsenkov@gmail.com>
2022-06-09 21:50:56 +0100

    Jump between ruby tests keybinding

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 -- example file i.e lua/custom/init.lua
 
 vim.api.nvim_exec([[ autocmd FileType go setlocal shiftwidth=8 softtabstop=8 expandtab ]], false)
+vim.api.nvim_create_autocmd("FileType ruby", { command= "map <leader>tt :RubyAlternate <CR>" })
