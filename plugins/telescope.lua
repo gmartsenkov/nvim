59600c7a31546f014a34c16302638c18109e362e
Georgi Martsenkov <g.martsenkov@gmail.com>
2022-06-14 23:29:45 +0100

    enable fzy-native

--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -62,17 +62,14 @@ local options = {
          },
       },
       extensions = {
-        fzf = {
-          fuzzy = false,                    -- false will only do exact matching
-          override_generic_sorter = true,  -- override the generic sorter
-          override_file_sorter = true,     -- override the file sorter
-          case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-          -- the default case_mode is "smart_case"
+        fzy_native = {
+          override_generic_sorter = false,
+          override_file_sorter = true,
         }
       }
    },
 
-   extensions_list = { "themes", "terms" },
+   extensions_list = { "themes", "terms", "fzy_native" },
 }
 
 local M = {}
