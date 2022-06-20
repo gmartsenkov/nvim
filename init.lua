6bf74d4cdf614dcd169485a2b95d8eb907bdf64e
Georgi Martsenkov <g.martsenkov@gmail.com>
2022-06-20 15:22:44 +0100

    Disable terminal line number

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 -- example file i.e lua/custom/init.lua
 
 vim.api.nvim_exec([[ autocmd FileType go setlocal shiftwidth=8 softtabstop=8 expandtab ]], false)
+vim.api.nvim_exec([[ autocmd TermOpen * setlocal nonumber norelativenumber ]], false)
