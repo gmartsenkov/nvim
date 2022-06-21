fc3f59262671df91e4f60fde3aa6ecb59b7b0825
Georgi Martsenkov <g.martsenkov@gmail.com>
2022-06-21 10:33:56 +0100

    Terminal auto insert mode

--- init.lua
+++ init.lua
@@ -2,3 +2,4 @@
 
 vim.api.nvim_exec([[ autocmd FileType go setlocal shiftwidth=8 softtabstop=8 expandtab ]], false)
 vim.api.nvim_exec([[ autocmd TermOpen * setlocal nonumber norelativenumber ]], false)
+vim.api.nvim_exec([[ autocmd TermOpen * startinsert ]], false)
