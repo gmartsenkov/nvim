f2cb0622195a55c39a82145be780479903b3c014
Georgi Martsenkov <g.martsenkov@gmail.com>
2023-03-28 11:26:39 +0100

    Increase toggleterm size

--- plugins.lua
+++ plugins.lua
@@ -9,7 +9,7 @@ return {
       open_mapping = [[<C-t>]],
       size = function(term)
         if term.direction == "horizontal" then
-          return 20
+          return 30
         elseif term.direction == "vertical" then
           return vim.o.columns * 0.4
         end
