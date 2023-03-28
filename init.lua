44dce404efa2cb74263967fc06389c01a56e9a11
Georgi Martsenkov <g.martsenkov@gmail.com>
2023-03-28 17:12:51 +0100

    Show relative line numbers

--- init.lua
+++ init.lua
@@ -14,5 +14,6 @@ if vim.g.neovide then
   vim.g.neovide_cursor_animation_length = 0
   vim.g.neovide_cursor_trail_size = 0
 end
+vim.wo.relativenumber = true
 -- vim.o.updatetime = 250
 -- vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
