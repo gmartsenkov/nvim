971e227d8c799d7902a7f27207846c9441d47ec6
Georgi Martsenkov <g.martsenkov@gmail.com>
2023-06-01 22:57:33 +0100

    Use default statusline

--- chadrc.lua
+++ chadrc.lua
@@ -7,45 +7,6 @@ local highlights = require "custom.highlights"
 M.ui = {
   theme = "gruvchad",
   theme_toggle = { "gruvchad_light", "gruvchad" },
-  statusline = {
-    overriden_modules = function()
-      local fn = vim.fn
-      local config = require("core.utils").load_config().ui.statusline
-      local sep_style = config.separator_style
-
-      local default_sep_icons = {
-        default = { left = "", right = " " },
-        round = { left = "", right = "" },
-        block = { left = "█", right = "█" },
-        arrow = { left = "", right = "" },
-      }
-
-      local separators = (type(sep_style) == "table" and sep_style) or default_sep_icons[sep_style]
-
-      local sep_l = separators["left"]
-      local sep_r = separators["right"]
-
-      return {
-        fileInfo = function()
-          local icon = " 󰈚 "
-          local filename = (fn.expand "%" == "" and "Empty ") or fn.expand "%"
-
-          if filename ~= "Empty " then
-            local devicons_present, devicons = pcall(require, "nvim-web-devicons")
-
-            if devicons_present then
-              local ft_icon = devicons.get_icon(filename)
-              icon = (ft_icon ~= nil and " " .. ft_icon) or ""
-            end
-
-            filename = " " .. filename .. " "
-          end
-
-          return "%#St_file_info#" .. icon .. filename .. "%#St_file_sep#" .. sep_r
-        end,
-      }
-    end,
-  },
   tabufline = {
     enabled = false,
   },
