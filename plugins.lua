04bb403c2cd3850e5beb25af493b1d6aaa3c5943
Georgi Martsenkov <g.martsenkov@gmail.com>
2023-06-05 16:30:45 +0100

    Add extra bindings in telescope

--- plugins.lua
+++ plugins.lua
@@ -192,6 +192,8 @@ return {
           ["<C-k>"] = require("telescope.actions").move_selection_previous,
           ["<C-n>"] = require("telescope.actions").cycle_history_next,
           ["<C-p>"] = require("telescope.actions").cycle_history_prev,
+          ["<C-a>"] = { "<home>", type = "command" },
+          ["<C-e>"] = { "<end>", type = "command" },
         },
       }
       c.defaults.file_ignore_patterns = { "node_modules", "resources/public/js/", ".git/", ".shadow-cljs/" }
