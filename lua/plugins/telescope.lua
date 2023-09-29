3e60d6a57c6165b84a6096c82d0b6ef007c2e250
Georgi Martsenkov <g.martsenkov@gmail.com>
2023-09-29 16:24:45 +0100

    Fix telescope config being nested in the wrong key, add extra snippets

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -17,42 +17,42 @@ return function()
         },
       },
       file_ignore_patterns = { "node_modules", "resources/public/js/", ".git/", ".shadow-cljs/" },
-      extensions = {
-        file_browser = {
-          mappings = {
-            i = {
-              ["<Tab>"] = actions.select_default,
-              ["<C-CR>"] = fb_actions.create_from_prompt
-            },
+    },
+    extensions = {
+      file_browser = {
+        mappings = {
+          i = {
+            ["<Tab>"] = actions.select_default,
+            ["<C-CR>"] = fb_actions.create_from_prompt
           },
         },
-        project = {
-          base_dirs = { "~/Development/", "~/.config/nvim/lua/" },
-          on_project_selected = function(prompt_bufnr)
-            -- Do anything you want in here. For example:
-            project_actions.change_working_directory(prompt_bufnr, false)
-            -- project_actions.find_project_files(prompt_bufnr, false)
-          end,
-        },
-        fzf = {
-          fuzzy = true, -- false will only do exact matching
-          override_generic_sorter = true, -- override the generic sorter
-          override_file_sorter = true, -- override the file sorter
-          case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-          -- the default case_mode is "smart_case"
-        },
-        advanced_git_search = {
-          -- fugitive or diffview
-          diff_plugin = "fugitive",
-          -- customize git in previewer
-          -- e.g. flags such as { "--no-pager" }, or { "-c", "delta.side-by-side=false" }
-          git_flags = {},
-          -- customize git diff in previewer
-          -- e.g. flags such as { "--raw" }
-          git_diff_flags = {},
-        },
       },
-      extensions_list = { "themes", "terms", "file_browser", "smart_history", "fzf", "project" }
-    }
+      project = {
+        base_dirs = { "~/Development/", "~/.config/nvim/lua/" },
+        on_project_selected = function(prompt_bufnr)
+          -- Do anything you want in here. For example:
+          project_actions.change_working_directory(prompt_bufnr, false)
+          -- project_actions.find_project_files(prompt_bufnr, false)
+        end,
+      },
+      fzf = {
+        fuzzy = false, -- false will only do exact matching
+        override_generic_sorter = true, -- override the generic sorter
+        override_file_sorter = true, -- override the file sorter
+        case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+        -- the default case_mode is "smart_case"
+      },
+      advanced_git_search = {
+        -- fugitive or diffview
+        diff_plugin = "fugitive",
+        -- customize git in previewer
+        -- e.g. flags such as { "--no-pager" }, or { "-c", "delta.side-by-side=false" }
+        git_flags = {},
+        -- customize git diff in previewer
+        -- e.g. flags such as { "--raw" }
+        git_diff_flags = {},
+      },
+    },
+    extensions_list = { "themes", "terms", "file_browser", "smart_history", "fzf", "project" }
   }
 end
