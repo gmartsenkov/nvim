179ba98a8a5a4a715537c51e175f92a81d687420
Georgi Martsenkov <g.martsenkov@gmail.com>
2024-05-11 22:36:02 +0100

    Add mini.surround

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,4 +1,5 @@
 return {
   {"echasnovski/mini.pairs", version = "*", lazy = false, config = true },
-  {"echasnovski/mini.comment", version = "*", lazy = false, config = true }
+  {"echasnovski/mini.comment", version = "*", lazy = false, config = true },
+  {"echasnovski/mini.surround", version = "*", lazy = false, config = true },
 }
